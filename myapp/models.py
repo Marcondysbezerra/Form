e56d0f6bf1f8090220fa0b0c9be6d73d70723cf5
55799 <marcondysbezerra@gmail.com>
2021-11-20 14:29:55 -0300

    Criada a classe Formulario em  models.py para receber as informações do formulário.

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Formulario(models.Model):
+    nome = models.CharField(max_length=50)
+    email = models.EmailField()
+    msg = models.TextField()
+
+    def __str__(self):
+        return self.nome
