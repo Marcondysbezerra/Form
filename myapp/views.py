136d0d71c600c1047ce106952d967d99b818e602
55799 <marcondysbezerra@gmail.com>
2021-11-22 21:39:15 -0300

    Criada a view para o formulário.

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from .forms import InputForm
 
 
 def formulario(request):
-    return render(request, 'myapp/formulario.html')
+
+    if request.method == "POST":
+        form = InputForm(request.POST)
+        if form.is_valid():
+            nome = form.cleaned_data.get('nome')
+            email = form.cleaned_data.get('email')
+            mensagem = form.cleaned_data.get('mensagem')
+            form.save()
+    form = InputForm()
+    msg = 'Alguma mensagem'
+    return render(request, 'myapp/formulario.html', {'form': form})
+
+
+
